0c57eb423c097476732d0d5bff819fcdf614c7e0
fatihkirli <51170589+fatihkirli@users.noreply.github.com>
2022-02-01 00:14:10 +0300

    Update OrderApplication.java

--- order/src/main/java/com/example/order/OrderApplication.java
+++ order/src/main/java/com/example/order/OrderApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderApplication {
 
 	public static void main(String[] args) {
